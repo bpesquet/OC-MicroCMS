dc9641c64e5d9767a2ffe225913dbc6fd32108b1
bpesquet <bpesquet@gmail.com>
2015-01-12 16:11:40 +0100

    Deployment on Heroku

--- app/config/prod.php
+++ app/config/prod.php
@@ -1,15 +1,19 @@
 <?php
 
+// Deployment on Heroku with ClearDB for MySQL
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
 // Doctrine (db)
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
-    'host'     => 'localhost',
+    'host'     => "$server",
     'port'     => '3306',
-    'dbname'   => 'microcms',
-    'user'     => 'microcms_user',
-    'password' => 'secret',
+    'dbname'   => "$db",
+    'user'     => "$username",
+    'password' => "$password",
 );
-
-// define log parameters
-$app['monolog.level'] = 'WARNING';
