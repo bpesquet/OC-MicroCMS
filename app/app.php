fd0cdeb7548242ce9791ce0ca2efe6ad8ac6ed24
bpesquet <bpesquet@gmail.com>
2015-10-26 12:08:26 +0100

    Route naming

--- app/app.php
+++ app/app.php
@@ -12,6 +12,7 @@ $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 // Register services
 $app['dao.article'] = $app->share(function ($app) {
--- app/routes.php
+++ app/routes.php
@@ -4,11 +4,11 @@
 $app->get('/', function () use ($app) {
     $articles = $app['dao.article']->findAll();
     return $app['twig']->render('index.html.twig', array('articles' => $articles));
-});
+})->bind('home');
 
 // Article details with comments
 $app->get('/article/{id}', function ($id) use ($app) {
     $article = $app['dao.article']->find($id);
     $comments = $app['dao.comment']->findAllByArticle($id);
     return $app['twig']->render('article.html.twig', array('article' => $article, 'comments' => $comments));
-});
+})->bind('article');;
