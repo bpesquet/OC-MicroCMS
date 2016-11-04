092633e5b09555805bef6d9cdec14599bcaa18aa
bpesquet <bpesquet@gmail.com>
2016-11-04 21:15:12 +0100

    Add debug mode for better error understanding

--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+// enable the debug mode
+$app['debug'] = true;
+
 require __DIR__.'/../app/routes.php';
 
 $app->run();
