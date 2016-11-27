ad5412881fbd3154cd4d1f0aeba93ccd14b27560
bpesquet <bpesquet@gmail.com>
2016-11-27 22:45:51 +0100

    Symphony change: use namespace to access form types

--- src/Form/Type/ArticleType.php
+++ src/Form/Type/ArticleType.php
@@ -4,14 +4,16 @@ namespace MicroCMS\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', 'text')
-            ->add('content', 'textarea');
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class);
     }
 
     public function getName()
