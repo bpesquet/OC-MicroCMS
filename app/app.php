891966cf4e2c6f696ee6d42f6cf0a7ec3f8a0862
bpesquet <bpesquet@gmail.com>
2016-12-17 22:44:27 +0100

    Remove web-profiler for stability reasons

--- app/app.php
+++ app/app.php
@@ -49,13 +49,13 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.name' => 'MicroCMS',
     'monolog.level' => $app['monolog.level']
 ));
-$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+/*$app->register(new Silex\Provider\ServiceControllerServiceProvider());
 if (isset($app['debug']) && $app['debug']) {
     $app->register(new Silex\Provider\HttpFragmentServiceProvider());
     $app->register(new Silex\Provider\WebProfilerServiceProvider(), array(
         'profiler.cache_dir' => __DIR__.'/../var/cache/profiler'
     ));
-}
+}*/
 
 // Register services
 $app['dao.article'] = function ($app) {
